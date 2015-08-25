42ba042464dff89b83b0addd56f1b918e71f04bf
mawenbao <mwenbao@gmail.com>
2015-08-25 09:31:49 +0800

    add article metadata option to control TOC

--- static/js/niu2.js
+++ static/js/niu2.js
@@ -31,6 +31,7 @@ window.gCurrTocId = '';
 window.gSidebarCtrlButtonEnabled = true;
 window.gMousePositionX = 0;
 window.gMousePositionY = 0;
+window.gTocStyle = 'fixed';
 
 $(document).ready(function() {
     initGoogleCSEAnimation();
@@ -49,18 +50,21 @@ function onContentLoaded() {
     //window.setInterval(updateFootnoteStatus, 500);
     updateFootnoteStatus();
 
-    // detach toc list before inserting many
-    // toc index <span>, for performance reason
-    getTocList().detach();
-    initTocListIndex(getTocList());
-    getTocList().appendTo(getSidebarToc());
+    window.gTocStyle = $('#niu2-toc').data('style');
+    if (window.gTocStyle != 'closed') {
+        // detach toc list before inserting many
+        // toc index <span>, for performance reason
+        getTocList().detach();
+        initTocListIndex(getTocList());
+        getTocList().appendTo(getSidebarToc());
 
-    setSidebarTocWidth();
-    setTocOverflowedTitle();
+        setSidebarTocWidth();
+        setTocOverflowedTitle();
 
-    initTocLinkScrollAnimation();
-    initAllTocsCtrl();
-    locateTocInViewport();
+        initTocLinkScrollAnimation();
+        initAllTocsCtrl();
+        locateTocInViewport();
+    }
 
     initToolbar();
 }
@@ -272,6 +276,9 @@ function resetSidebarToc() {
 }
 
 function toggleSidebarTocFixed() {
+    if (window.gTocStyle != 'fixed') {
+        return;
+    }
     var sidebarToc = $('#niu2-sidebar-toc');
     var sidebarMeta = $('#niu2-sidebar-meta');
     if (sidebarToc.length == 0 || sidebarMeta.length == 0) {
@@ -448,6 +455,9 @@ function getHtmlHeaders() {
 }
 
 function locateTocInViewport() {
+    if (window.gTocStyle != 'fixed') {
+        return;
+    }
     if (!window.gEnableTocStatusUpdate) {
         return;
     }
@@ -488,6 +498,9 @@ function getSidebarToc() {
 }
 
 function getTocList() {
+    if (window.gTocStyle == 'closed') {
+        return null;
+    }
     if (!window.gTocList) {
         window.gTocList = $('#niu2-sidebar-toc-list');
         window.gTocListType = window.gTocList[0].tagName.toLowerCase();
@@ -967,10 +980,12 @@ function initMouseXYRecord() {
         }
 
         // sidebar toc list scroll
-        if (isPositionInRect(e.clientX, e.clientY, getTocList()[0].getBoundingClientRect())) {
-            window.gMouseInSidebarTocList = true;
-        } else {
-            window.gMouseInSidebarTocList = false;
+        if (window.gTocStyle != 'closed') {
+            if (isPositionInRect(e.clientX, e.clientY, getTocList()[0].getBoundingClientRect())) {
+                window.gMouseInSidebarTocList = true;
+            } else {
+                window.gMouseInSidebarTocList = false;
+            }
         }
 
         // toggle toolbar
